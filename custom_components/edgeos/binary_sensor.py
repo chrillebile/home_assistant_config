9c4c4cf082e1ab08eea170c73984e5855916388b
Christian Bilevits <chrillebile@users.noreply.github.com>
2021-10-10 21:09:41 +0200

    Upgrade HA to 2021.10.2

---
+++ custom_components/edgeos/binary_sensor.py
@@ -0,0 +1,62 @@
+"""
+Support for EdgeOS binary sensors.
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/binary_sensor.edgeos/
+"""
+import logging
+
+from homeassistant.const import STATE_OFF, STATE_ON
+from homeassistant.core import HomeAssistant
+
+from .helpers.const import *
+from .models.base_entity import EdgeOSEntity, async_setup_base_entry
+from .models.entity_data import EntityData
+
+_LOGGER = logging.getLogger(__name__)
+
+CURRENT_DOMAIN = DOMAIN_BINARY_SENSOR
+
+
+def get_binary_sensor(hass: HomeAssistant, integration_name: str, entity: EntityData):
+    binary_sensor = EdgeOSBinarySensor()
+    binary_sensor.initialize(hass, integration_name, entity, CURRENT_DOMAIN)
+
+    return binary_sensor
+
+
+async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities):
+    """Set up EdgeOS based off an entry."""
+    await async_setup_base_entry(
+        hass, entry, async_add_entities, CURRENT_DOMAIN, get_binary_sensor
+    )
+
+
+async def async_unload_entry(hass, config_entry):
+    _LOGGER.info(f"async_unload_entry {CURRENT_DOMAIN}: {config_entry}")
+
+    return True
+
+
+class EdgeOSBinarySensor(EdgeOSEntity):
+    """Representation a binary sensor that is updated by EdgeOS."""
+
+    @property
+    def is_on(self):
+        """Return true if the binary sensor is on."""
+        return bool(self.entity.state)
+
+    @property
+    def state(self):
+        """Return the state of the binary sensor."""
+        return STATE_ON if self.is_on else STATE_OFF
+
+    async def async_added_to_hass_local(self):
+        _LOGGER.info(f"Added new {self.name}")
+
+    def _immediate_update(self, previous_state: bool):
+        if previous_state != self.entity.state:
+            _LOGGER.debug(
+                f"{self.name} updated from {previous_state} to {self.entity.state}"
+            )
+
+        super()._immediate_update(previous_state)
---
+++ custom_components/edgeos/device_tracker.py
@@ -0,0 +1,65 @@
+"""
+Support for Ubiquiti EdgeOS routers.
+HEAVILY based on the AsusWRT component
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/device_tracker.edgeos/
+"""
+import logging
+
+from homeassistant.components.device_tracker import ATTR_SOURCE_TYPE, SOURCE_TYPE_ROUTER
+from homeassistant.components.device_tracker.config_entry import ScannerEntity
+from homeassistant.core import HomeAssistant
+
+from .helpers.const import *
+from .models.base_entity import EdgeOSEntity, async_setup_base_entry
+from .models.entity_data import EntityData
+
+_LOGGER = logging.getLogger(__name__)
+DEPENDENCIES = [DOMAIN]
+
+CURRENT_DOMAIN = DOMAIN_DEVICE_TRACKER
+
+
+def get_device_tracker(hass: HomeAssistant, integration_name: str, entity: EntityData):
+    device_tracker = EdgeOSScanner()
+    device_tracker.initialize(hass, integration_name, entity, CURRENT_DOMAIN)
+
+    return device_tracker
+
+
+async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities):
+    """Set up EdgeOS based off an entry."""
+    await async_setup_base_entry(
+        hass, entry, async_add_entities, CURRENT_DOMAIN, get_device_tracker
+    )
+
+
+async def async_unload_entry(hass, config_entry):
+    _LOGGER.info(f"async_unload_entry {CURRENT_DOMAIN}: {config_entry}")
+
+    return True
+
+
+class EdgeOSScanner(EdgeOSEntity, ScannerEntity):
+    """Represent a tracked device."""
+
+    @property
+    def is_connected(self):
+        """Return true if the device is connected to the network."""
+        return self.entity.state
+
+    @property
+    def source_type(self):
+        """Return the source type, eg gps or router, of the device."""
+        return self.entity.attributes.get(ATTR_SOURCE_TYPE, SOURCE_TYPE_ROUTER)
+
+    async def async_added_to_hass_local(self):
+        _LOGGER.info(f"Added new {self.name}")
+
+    def _immediate_update(self, previous_state: bool):
+        if previous_state != self.entity.state:
+            _LOGGER.debug(
+                f"{self.name} updated from {previous_state} to {self.entity.state}"
+            )
+
+        super()._immediate_update(previous_state)
